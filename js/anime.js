eda2e069567e51b384d8287acec7a060886212b1
AlieZ <84179586+Minerva-Main@users.noreply.github.com>
2024-03-19 17:55:31 +0800

    anime.js

--- js/anime.js
+++ js/anime.js
@@ -1,6 +1,6 @@
 // Api urls
 
-const ProxyApi = "https://athenaproject.chaossecmatthew.workers.dev/";
+const ProxyApi = "https://proxy.techzbots1.workers.dev/?u=https://projectathena.pages.dev/";
 const animeapi = "/anime/";
 const recommendationsapi = "/recommendations/";
 
